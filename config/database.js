c0e5c4ff14b671225c1d193bda8f02523473a0ed
Gulácsi András <guland@protonmail.com>
2022-08-20 03:02:23 +0200

    Prepare strapi backend for heroku deployment

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'starcity'),
+      user: env('DATABASE_USERNAME', 'gulacsi'),
+      password: env('DATABASE_PASSWORD', 'alma5578'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
   },
 });
